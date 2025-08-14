a518f3a2ce501078cff0242cba39c8aaf2ce9bd9
Safargulov Ravil <safargulov_ravil@mail.ru>
2025-08-14 13:26:06 +0500

    feat(test) add UserDaoImplTest and UserServiceTest

--- src/test/java/homework/dao/UserDaoImplTest.java
+++ src/test/java/homework/dao/UserDaoImplTest.java
@@ -1,4 +1,176 @@
-import junit.framework.TestCase;
-public class UserDaoImplTest extends TestCase {
-  
+package homework.dao;
+
+import homework.config.TestContainerConfig;
+import homework.entity.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Timestamp;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserDaoImplTest {
+
+    private static UserDao userDao;
+
+    @BeforeAll
+    static void setUpBeforeClass() {
+        userDao = new UserDaoImpl(TestContainerConfig.getSessionFactory());
+    }
+
+    @AfterAll
+    static void setUpAfterClass() {
+        TestContainerConfig.stop();
+    }
+
+    @Test
+    public void testSaveUser() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setName("Ravil1");
+        user.setAge(23);
+        user.setEmail("@eagle1");
+        user.setCreatedAt(created);
+        userDao.saveUser(user);
+
+        User foundUser = userDao.getUserById(user.getId()).orElse(null);
+        assertNotNull(foundUser);
+        assertEquals(foundUser.getName(), user.getName());
+        assertEquals(foundUser.getAge(), user.getAge());
+        assertEquals(created.getTime(), foundUser.getCreatedAt().getTime());
+    }
+
+    @Test
+    public void testSaveUserThrowException() {
+        RuntimeException thrown = assertThrows(
+                RuntimeException.class,
+                () -> userDao.saveUser(null),
+                "Ожидалось исключение RuntimeException при попытке сохранить null объект User"
+        );
+
+        assertTrue(thrown.getMessage().contains("Ошибка при добавлении пользователя"));
+        assertInstanceOf(IllegalArgumentException.class, thrown.getCause());
+    }
+
+    @Test
+    public void testGetUsers() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setName("Ravil2");
+        user.setAge(23);
+        user.setEmail("@eagle2");
+        user.setCreatedAt(created);
+
+        userDao.saveUser(user);
+
+        assertEquals(1, userDao.getUsers().size());
+    }
+
+    @Test
+    public void testGetUserById() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setName("Ravil3");
+        user.setAge(23);
+        user.setEmail("@eagle3");
+        user.setCreatedAt(created);
+
+        userDao.saveUser(user);
+        long savedId = user.getId();
+
+        User foundUser = userDao.getUserById(savedId).orElse(null);
+
+        assertNotNull(foundUser);
+        assertEquals(user.getName(), foundUser.getName());
+        assertEquals(user.getAge(), foundUser.getAge());
+        assertEquals(created.getTime(), foundUser.getCreatedAt().getTime());
+    }
+
+    @Test
+    public void testGetUserByIdThrowException() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setName("Ravil4");
+        user.setAge(23);
+        user.setEmail("@eagle4");
+        user.setCreatedAt(created);
+
+        userDao.saveUser(user);
+
+        Optional<User> foundUser = userDao.getUserById(999L);
+        assertFalse(foundUser.isPresent(), "Пользователь с несуществующим ID не должен быть найден");
+    }
+
+    @Test
+    public void testUpdateUser() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setName("Ravil5");
+        user.setAge(23);
+        user.setEmail("@eagle5");
+        user.setCreatedAt(created);
+        userDao.saveUser(user);
+
+        User updatedUser = new User();
+        updatedUser.setId(user.getId());
+        updatedUser.setName("Ravil5");
+        updatedUser.setAge(24);
+        updatedUser.setEmail("@eagle5");
+        updatedUser.setCreatedAt(created);
+
+        userDao.updateUser(updatedUser);
+
+        User foundUser = userDao.getUserById(updatedUser.getId()).orElse(null);
+        assertNotNull(foundUser);
+        assertEquals(updatedUser.getName(), foundUser.getName());
+        assertEquals(updatedUser.getAge(), foundUser.getAge());
+        assertEquals(created.getTime(), foundUser.getCreatedAt().getTime());
+    }
+
+    @Test
+    public void updateUserThrowException() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User updatedUser = new User();
+        updatedUser.setId(999L);
+        updatedUser.setName("Ravil Updated");
+        updatedUser.setAge(24);
+        updatedUser.setEmail("@eagle6");
+        updatedUser.setCreatedAt(created);
+
+        RuntimeException thrown = assertThrows(
+                RuntimeException.class,
+                () -> userDao.updateUser(updatedUser),
+                "Ожидалось исключение RuntimeException при попытке обновить несуществующего пользователя"
+        );
+        assertTrue(thrown.getMessage().contains("Ошибка при обновлении пользователя"));
+    }
+
+    @Test
+    public void testDeleteUser() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setName("Ravil7");
+        user.setAge(23);
+        user.setEmail("@eagle7");
+        user.setCreatedAt(created);
+        long savedId = user.getId();
+
+        userDao.deleteUser(user);
+
+        Optional<User> foundUser = userDao.getUserById(savedId);
+        assertFalse(foundUser.isPresent(), "Пользователь должен быть удалён из базы");
+    }
+
+    @Test
+    public void testDeleteUserThrowException() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> userDao.deleteUser(null),
+                "Ожидалось исключение IllegalArgumentException при попытке удалить null пользователя"
+        );
+        assertEquals("Пользователь не может быть null", thrown.getMessage());
+    }
+
 }
--- src/test/java/homework/service/UserServiceTest.java
+++ src/test/java/homework/service/UserServiceTest.java
@@ -1,4 +1,191 @@
-import junit.framework.TestCase;
-public class UserServiceTest extends TestCase {
-  
+package homework.service;
+
+import homework.dao.UserDao;
+import homework.entity.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserDao userDaoMock;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void testGetAllUsers() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setName("Ravil");
+        user.setAge(23);
+        user.setEmail("@eagle");
+        user.setCreatedAt(created);
+        List<User> users = new ArrayList<>();
+        users.add(user);
+
+        when(userDaoMock.getUsers()).thenReturn(users);
+        List<User> allUsers = userService.getAllUsers();
+
+        assertNotNull(allUsers);
+        assertEquals(allUsers.size(), users.size());
+    }
+
+    @Test
+    public void testGetUserById() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setId(1L);
+        user.setName("Ravil");
+        user.setAge(23);
+        user.setEmail("@eagle");
+        user.setCreatedAt(created);
+
+        when(userDaoMock.getUserById(1L)).thenReturn(Optional.of(user));
+        User userById = userService.getUserById(1L);
+
+        assertNotNull(userById);
+
+        assertEquals(userById.getName(), user.getName());
+        assertEquals(userById.getAge(), user.getAge());
+        assertEquals(userById.getEmail(), user.getEmail());
+        assertEquals(userById.getCreatedAt(), user.getCreatedAt());
+    }
+
+    @Test
+    public void testGetUserByIdThrowsException() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> userService.getUserById(-1L),
+                "Ожидалось исключение IllegalArgumentException при попытке передать отрицательный id");
+        assertTrue(thrown.getMessage().contains("ID должен быть положительным числом"));
+    }
+
+    @Test
+    public void testAddUser() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setId(1L);
+        user.setName("Ravil");
+        user.setAge(23);
+        user.setEmail("@eagle");
+        user.setCreatedAt(created);
+
+        userService.addUser(user);
+
+        verify(userDaoMock).saveUser(user);
+    }
+
+    @Test
+    void testAddUserWithNullUser() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> userService.addUser(null));
+        assertEquals("Пользователь не может быть null", thrown.getMessage());
+    }
+
+    @Test
+    void testAddUserWithEmptyName() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setId(1L);
+        user.setName(" ");
+        user.setAge(23);
+        user.setEmail("@eagle");
+        user.setCreatedAt(created);
+
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> userService.addUser(user));
+        assertEquals("Имя пользователя не может быть пустым", thrown.getMessage());
+    }
+
+    @Test
+    void testAddUserWithInvalidEmail() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setId(1L);
+        user.setName("Ravil");
+        user.setAge(23);
+        user.setEmail("eagle");
+        user.setCreatedAt(created);
+
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> userService.addUser(user));
+        assertEquals("Некорректный email", thrown.getMessage());
+    }
+
+    @Test
+    void testAddUserWithInvalidAge() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setId(1L);
+        user.setName("Ravil");
+        user.setAge(-23);
+        user.setEmail("@eagle");
+        user.setCreatedAt(created);
+
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> userService.addUser(user));
+        assertEquals("Возраст должен быть положительным числом", thrown.getMessage());
+    }
+
+    @Test
+    public void testUpdateUser() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setId(1L);
+        user.setName("Ravil");
+        user.setAge(23);
+        user.setEmail("@eagle");
+        user.setCreatedAt(created);
+
+        userService.updateUser(user);
+
+        verify(userDaoMock).updateUser(user);
+    }
+
+    @Test
+    public void testDeleteUser() {
+        Timestamp created = new Timestamp(System.currentTimeMillis());
+        User user = new User();
+        user.setId(1L);
+        user.setName("Ravil");
+        user.setAge(23);
+        user.setEmail("@eagle");
+        user.setCreatedAt(created);
+
+        when(userDaoMock.getUserById(user.getId())).thenReturn(Optional.of(user));
+        userService.deleteUser(user.getId());
+
+        verify(userDaoMock).deleteUser(user);
+    }
+
+    @Test
+    public void testDeleteUserThrowsException() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> userService.deleteUser(-1L),
+                "Ожидалось исключение IllegalArgumentException при попытке передать отрицательный id");
+        assertTrue(thrown.getMessage().contains("ID должен быть положительным числом"));
+    }
+
+    @Test
+    public void testDeleteUserNullThrowsException() {
+        when(userDaoMock.getUserById(1L)).thenReturn(Optional.empty());
+
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> userService.deleteUser(1L),
+                "Ожидалось исключение IllegalArgumentException при попытке передать id отсутствующего пользователя"
+                );
+        assertEquals("Пользователь с ID 1 не найден", thrown.getMessage());
+    }
 }
